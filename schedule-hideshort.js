f9156d6e25f3ab0e590a2fa816c92c0d74d7c72c
aaronlevi <aaronlevi.garcia@gmail.com>
2020-11-17 17:10:01 -0500

    Added french url support for pages and shortened code

--- schedule-hideshort.js
+++ schedule-hideshort.js
@@ -1,6 +1,6 @@
       /*Navigate to French version for all pages.*/
       window.onload = function() {      
-        var frHost = "http://www.fr.acff.ca";
+        var frHost = "https://www.fr.acff.ca";
         var currentPage = location.pathname;
         var frenchLink = document.getElementById("fr");
         var frenchUrl = frHost + currentPage;
@@ -10,117 +10,38 @@
       }
       /*Program Calendar Toggle movies schedule by event day.*/
       var scheduleButtons = document.querySelectorAll('.calendar-option');
-
+      
       scheduleButtons.forEach(scheduleButton => {
         scheduleButton.addEventListener("click", function() {
 
-          var eventChosen = this.id;
-          //hide scheduled movies by day
-          const hide = (e) => {
-            e.style.display = 'none'
-          };
-          //show scheduled movies by day
-          const show = (e) => {
-            e.style.display = 'block'
-          };
-          //Change background to black on active schedule buttons
-          const darkBackground = (e) => {
-            e.style.backgroundColor = "#000000";
-          };
-          //Change background back to gray on inactive schedule buttons
-          const lightBackground = (e) => {
-            e.style.backgroundColor = "#666666";
-          };
-
+          var eventChosen = this.id;          
           const events = document.querySelectorAll('.calendar-option');
           const screenings = document.querySelectorAll('.movies-wrapper');
-            //Detect which event is clicked on the schedule page
-            switch (eventChosen) {
-              //if first screening is chosen
-              case "event-1":
-              //change color of buttons
+          //hide scheduled movies by day
+          const hide = (elem) => {
+            screenings.forEach(screening => {
+              screening.style.display = 'none';
+            });
+          };
+          //show scheduled movies by day
+          const show = (elem) => {
+            document.getElementById(elem).style.display = 'block'
+          };
+          //Change background to black on active schedule buttons
+          const darkBackground = (elem) => {
+            document.getElementById(elem).style.backgroundColor = "#ffdf00";
+          };
+          //Change background back to gray on inactive schedule buttons
+          const lightBackground = (elem) => {            
               events.forEach(event => {
-                lightBackground(event); 
-              });
-                darkBackground(document.getElementById(this.id));
-              //hide and show movies for each schedule element
-              screenings.forEach(screening => {
-                hide(screening);
-              });
-                show(document.getElementById('one'));
-                break;
+                 event.style.backgroundColor = "#ffffff";
+              });  
+          };
 
-                case "event-2":
-                events.forEach(event => {
-                  lightBackground(event);
-                });
-                  darkBackground(document.getElementById(this.id));
+          lightBackground();
+          darkBackground(this.id);
+          hide();
+          show('event-' + this.id);
 
-                screenings.forEach(screening => {
-                  hide(screening);
-                });
-                  show(document.getElementById('two'));
-                  break;
-
-                case "event-3":
-                events.forEach (event => {
-                  lightBackground(event);
-                });
-                  darkBackground(document.getElementById(this.id));
-
-                  screenings.forEach(screening => {
-                    hide(screening);
-                  });
-                    show(document.getElementById('three'));
-                  break;
-
-                case "event-4":
-                  events.forEach(event => {
-                    lightBackground(event);
-                  });
-                    darkBackground(document.getElementById(this.id));  
-
-                  screenings.forEach(screening => {
-                    hide(screening);
-                    });
-                    show(document.getElementById('four'));
-                  break;
-
-                  case "event-5":
-                    events.forEach(event => {
-                      lightBackground(event);
-                    });
-                      darkBackground(document.getElementById(this.id));
-
-                    screenings.forEach(screening => {
-                      hide(screening);
-                    });  
-                      show(document.getElementById('five'));
-                    break;
-
-                  case "event-6":
-                    events.forEach(event => {
-                      lightBackground(event);
-                    });
-                      darkBackground(document.getElementById(this.id));
-                    
-                    screenings.forEach(screening => {
-                      hide(screening);
-                    });
-                      show(document.getElementById('six'));
-                    break;
-
-                  case "event-7":
-                    events.forEach(event => {
-                      lightBackground(event);
-                    });
-                      darkBackground(document.getElementById(this.id));
-                    
-                    screenings.forEach(screening => {
-                      hide(screening);
-                    });  
-                      show(document.getElementById('seven'));
-                    break;
-            }
         });
       });
