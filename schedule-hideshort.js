a92fa77546970dd7caac8073225e3aabd46fae9a
aaronlevi <aaronlevi.garcia@gmail.com>
2020-11-17 18:13:37 -0500

    removed the word var

--- schedule-hideshort.js
+++ schedule-hideshort.js
@@ -1,20 +1,20 @@
       /*Navigate to French version for all pages.*/
       window.onload = function() {      
-        var frHost = "https://www.fr.acff.ca";
-        var currentPage = location.pathname;
-        var frenchLink = document.getElementById("fr");
-        var frenchUrl = frHost + currentPage;
+        const frHost = "https://www.fr.acff.ca";
+        const currentPage = location.pathname;
+        const frenchLink = document.getElementById("fr");
+        const frenchUrl = frHost + currentPage;
         document.getElementById("fr").addEventListener("click", function() {         
              frenchLink.href = frenchUrl;
         });
       }
       /*Program Calendar Toggle movies schedule by event day.*/
-      var scheduleButtons = document.querySelectorAll('.calendar-option');
+      const scheduleButtons = document.querySelectorAll('.calendar-option');
       
       scheduleButtons.forEach(scheduleButton => {
         scheduleButton.addEventListener("click", function() {
 
-          var eventChosen = this.id;          
+          const eventChosen = this.id;          
           const events = document.querySelectorAll('.calendar-option');
           const screenings = document.querySelectorAll('.movies-wrapper');
           //hide scheduled movies by day
